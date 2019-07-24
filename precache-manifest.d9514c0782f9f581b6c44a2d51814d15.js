9c6be43b9a4383ab769c5d18874cc5d7b8e271e9
Robin Flygare <robin.flygare1@gmail.com>
2019-07-24 19:58:32 +0200

    Updates

---
+++ precache-manifest.d9514c0782f9f581b6c44a2d51814d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0692f667718da4930fed2e69989f5ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f74855a13e8b111d6ff",
+    "url": "/static/css/2.18f57b05.chunk.css"
+  },
+  {
+    "revision": "32ecf7e51d54a60175ba",
+    "url": "/static/css/main.f3b718c0.chunk.css"
+  },
+  {
+    "revision": "6f74855a13e8b111d6ff",
+    "url": "/static/js/2.e782c4e2.chunk.js"
+  },
+  {
+    "revision": "32ecf7e51d54a60175ba",
+    "url": "/static/js/main.b2c804bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
